749e3d71a0f4597f2c55b5f7c95fb7c57f60fee9
aolowude <aolowude@gmail.com>
2023-11-15 11:01:32 +0000

    Invoices

--- dashboard/app/dashboard/invoices/page.tsx
+++ dashboard/app/dashboard/invoices/page.tsx
@@ -17,11 +17,6 @@ export default async function Page() {
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Invoices
       </h1>
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Suspense fallback={<CardsSkeleton />}>
-          <CardWrapper />
-        </Suspense>
-      </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-4">
         <Suspense fallback={<LatestInvoicesSkeleton />}>
           <LatestInvoices />
