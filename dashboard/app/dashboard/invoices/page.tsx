650e7b7fccc0c7b3c6ace9be959a5bd03b513585
aolowude <aolowude@gmail.com>
2023-11-15 11:03:23 +0000

    Latest Invoices

--- dashboard/app/dashboard/invoices/page.tsx
+++ dashboard/app/dashboard/invoices/page.tsx
@@ -14,9 +14,6 @@ import {
 export default async function Page() {
   return (
     <main>
-      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Invoices
-      </h1>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-4">
         <Suspense fallback={<LatestInvoicesSkeleton />}>
           <LatestInvoices />
