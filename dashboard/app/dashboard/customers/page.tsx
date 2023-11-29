b1b810f04e6942d0be287227c8b6a16fbe3a65db
aolowude <aolowude@gmail.com>
2023-11-29 14:28:14 +0100

    Doctor and Customer view
    Change the field shown

--- dashboard/app/dashboard/customers/page.tsx
+++ dashboard/app/dashboard/customers/page.tsx
@@ -2,7 +2,7 @@ const people = [
   {
     name: "Leslie Alexander",
     email: "leslie.alexander@example.com",
-    role: "Patient",
+    role: "Dr Susan Obika",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     lastSeen: "3h ago",
@@ -11,7 +11,7 @@ const people = [
   {
     name: "Michael Foster",
     email: "michael.foster@example.com",
-    role: "Referral",
+    role: "Dr David Osha",
     imageUrl:
       "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     lastSeen: "3h ago",
@@ -20,7 +20,7 @@ const people = [
   {
     name: "Dries Vincent",
     email: "dries.vincent@example.com",
-    role: "Consultation",
+    role: "Dr Susan Obika",
     imageUrl:
       "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     lastSeen: null,
--- dashboard/app/dashboard/doctors/page.tsx
+++ dashboard/app/dashboard/doctors/page.tsx
@@ -2,7 +2,7 @@ const people = [
   {
     name: "Susan Obika",
     email: "susan.obika@example.com",
-    role: "Doctor",
+    role: "2 patients",
     imageUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     lastSeen: "3h ago",
@@ -11,7 +11,7 @@ const people = [
   {
     name: "David Osha",
     email: "michael.osha@example.com",
-    role: "Surgeon",
+    role: "1 patient",
     imageUrl:
       "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     lastSeen: "3h ago",
